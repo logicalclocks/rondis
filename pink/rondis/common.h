0ae69df18dd1e99d19c10894a2074c7eb9269218
Vincent Lohse <vincent.lohse@logicalclocks.com>
2024-10-29 17:18:53 +0000

    Streamlined top-level errors

--- pink/rondis/common.h
+++ pink/rondis/common.h
@@ -14,6 +14,7 @@ int execute_no_commit(NdbTransaction *trans, int &ret_code, bool allow_fail);
 int execute_commit(Ndb *ndb, NdbTransaction *trans, int &ret_code);
 int write_formatted(char *buffer, int bufferSize, const char *format, ...);
 void assign_ndb_err_to_response(std::string *response, const char *app_str, Uint32 error_code);
+void assign_generic_err_to_response(std::string *response,const char *app_str);
 
 // NDB API error messages
 #define FAILED_GET_DICT "Failed to get NdbDict"
@@ -23,6 +24,10 @@ void assign_ndb_err_to_response(std::string *response, const char *app_str, Uint
 #define FAILED_READ_KEY "Failed to read key"
 #define FAILED_GET_OP "Failed to get NdbOperation object"
 #define FAILED_DEFINE_OP "Failed to define RonDB operation"
+
+// Redis errors
+#define UNKNOWN_COMMAND "unknown command '%s'"
+#define WRONG_NUMBER_OF_ARGS "wrong number of arguments for '%s' command"
 
 // Generic errors
 void failed_no_such_row_error(std::string *response);
